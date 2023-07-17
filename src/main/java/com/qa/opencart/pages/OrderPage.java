60cff5c5fc31fb936363b96c4a2faa5f405242fb
unknown <kazemianazimeh@gmail.com>
2023-07-17 13:53:19 -0400

    added order page

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class OrderPage {
+
+	public static void main(String[] args) {
+
+		By locator =By.id("order");
+		System.out.println("making new branch in my git");
+		
+	}
+
+}
